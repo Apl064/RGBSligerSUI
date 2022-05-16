9cf9bba97b166360589474246fd05aea0cebea66
apl064 <apl064@gmail.com>
2022-05-16 21:13:59 +0300

    Initial Commit

---
+++ RGBSligerSUI/RGBSligerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSligerSUIApp.swift
+//  RGBSligerSUI
+//
+//  Created by Pavel Metelin on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSligerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
