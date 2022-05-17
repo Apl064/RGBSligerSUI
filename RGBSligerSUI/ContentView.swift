3da6622c1a61a6db6dd56c7c840f7e1971e93743
apl064 <apl064@gmail.com>
2022-05-17 23:42:49 +0300

    final

--- RGBSligerSUI/ContentView.swift
+++ RGBSligerSUI/ContentView.swift
@@ -7,70 +7,142 @@
 
 import SwiftUI
 
+private enum RGBTextField {
+    case redTF
+    case greenTF
+    case blueTF
+}
+
 struct ContentView: View {
-    @State private var isPresented = false
-    
-    @State private var sliderValue =  randomSliderValue()//Double.random(in: 0...255)
-    
-    @State private var tempText = "333"
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    @State private var redTFValue = ""
+    @State private var greenTFValue = ""
+    @State private var blueTFValue = ""
     
     var body: some View {
         
         ZStack {
             backgroudColor
-            
             VStack(spacing: 40) {
-                RGBView(red: sliderValue,
-                        green: sliderValue,
-                        blue: sliderValue)
-                
+                RGBView(red: redSliderValue,
+                        green: greenSliderValue,
+                        blue: blueSliderValue)
                 VStack(spacing: 10) {
-//                    CustomSliderView(value: $sliderValue,
-//                                     valueText: $sliderValue, color: .red)
+                    
                     HStack(spacing: 8) {
-                        Text("R: \(lround(sliderValue))")
-                            .frame(width: 55, height: 30, alignment: .leading)
-                        Text("0")
-                            .foregroundColor(.red)
-                        Slider(value: $sliderValue, in: 0...255, step: 1)
+                        Text("\(lround(redSliderValue))")
+                            .frame(width: 35, height: 30, alignment: .leading)
+                        Slider(value: $redSliderValue, in: 0...255, step: 1)
                             .tint(.red)
-                        Text("255")
-                            .foregroundColor(.red)
-                        TextField(valueTextField: $tempText)
+                            .onChange(of: redSliderValue) { newValue in
+                                redTFValue = "\(lround(redSliderValue))"
+                            }
+                            .onAppear {
+                                redTFValue = "\(lround(redSliderValue))"
+                            }
+                        TextField("", text: $redTFValue)
                             .frame(width: 35, height: 30, alignment: .center)
                             .border(.black, width: 1)
+                            .onSubmit {
+                                checkTFValue(inputText: redTFValue,
+                                             txtField: .redTF)
+                            }
+                            .onChange(of: redTFValue) { newValue in
+                                guard let isNum = Double(redTFValue) else {
+                                    redTFValue = ""
+                                    return
+                                }
+                                redTFValue = String(format: "%.f", isNum)
+                            }
                     }.padding(.horizontal, 8)
                     
                     HStack(spacing: 8) {
-                        Text("G: \(lround(sliderValue))")
-                            .frame(width: 55, height: 30, alignment: .leading)
-                        Text("0")
-                            .foregroundColor(.green)
-                        Slider(value: $sliderValue, in: 0...255, step: 1)
+                        Text("\(lround(greenSliderValue))")
+                            .frame(width: 35, height: 30, alignment: .leading)
+                        Slider(value: $greenSliderValue, in: 0...255, step: 1)
                             .tint(.green)
-                        Text("255")
-                            .foregroundColor(.green)
-                        TextField(valueTextField: $tempText)
+                            .onChange(of: greenSliderValue) { newValue in
+                                greenTFValue = "\(lround(greenSliderValue))"
+                            }
+                            .onAppear {
+                                greenTFValue = "\(lround(greenSliderValue))"
+                            }
+                        TextField("", text: $greenTFValue)
                             .frame(width: 35, height: 30, alignment: .center)
                             .border(.black, width: 1)
+                            .onSubmit {
+                                checkTFValue(inputText: greenTFValue,
+                                             txtField: .greenTF)
+                            }
+                            .onChange(of: greenTFValue) { newValue in
+                                guard let isNum = Double(greenTFValue) else {
+                                    greenTFValue = ""
+                                    return
+                                }
+                                greenTFValue = String(format: "%.f", isNum)
+                            }
                     }.padding(.horizontal, 8)
                     
                     HStack(spacing: 8) {
-                        Text("B: \(lround(sliderValue))")
-                            .frame(width: 55, height: 30, alignment: .leading)
-                        Text("0")
-                            .foregroundColor(.blue)
-                        Slider(value: $sliderValue, in: 0...255, step: 1)
+                        Text("\(lround(blueSliderValue))")
+                            .frame(width: 35, height: 30, alignment: .leading)
+                        Slider(value: $blueSliderValue, in: 0...255, step: 1)
                             .tint(.blue)
-                        Text("255")
-                            .foregroundColor(.blue)
-                        TextField(valueTextField: $tempText)
+                            .onChange(of: blueSliderValue) { newValue in
+                                blueTFValue = "\(lround(blueSliderValue))"
+                            }
+                            .onAppear {
+                                blueTFValue = "\(lround(blueSliderValue))"
+                            }
+                        TextField("", text: $blueTFValue)
                             .frame(width: 35, height: 30, alignment: .center)
-                            .border(.black, width: 1).tint(.red)
+                            .border(.black, width: 1)
+                            .onSubmit {
+                                checkTFValue(inputText: blueTFValue,
+                                             txtField: .blueTF)
+                            }
+                            .onChange(of: blueTFValue) { newValue in
+                                guard let isNum = Double(blueTFValue) else {
+                                    blueTFValue = ""
+                                    return
+                                }
+                                blueTFValue = String(format: "%.f", isNum)
+                            }
                     }.padding(.horizontal, 8)
-            }
+                }
                 Spacer()
             }.padding()
+        }
+    }
+    
+    private func checkTFValue(inputText: String, txtField: RGBTextField) {
+        switch txtField {
+        case .redTF:
+            guard let newNumber = Double(inputText), newNumber <= 255 else {
+                redTFValue = String(255)
+                redSliderValue = 255
+                return
+            }
+            redTFValue = String(format: "%.f", newNumber)
+            redSliderValue = newNumber
+        case .greenTF:
+            guard let newNumber = Double(inputText), newNumber <= 255 else {
+                greenTFValue = String(255)
+                greenSliderValue = 255
+                return
+            }
+            greenTFValue = String(format: "%.f", newNumber)
+            greenSliderValue = newNumber
+        case .blueTF:
+            guard let newNumber = Double(inputText), newNumber <= 255 else {
+                blueTFValue = String(255)
+                blueSliderValue = 255
+                return
+            }
+            blueTFValue = String(format: "%.f", newNumber)
+            blueSliderValue = newNumber
         }
     }
 }
@@ -81,10 +153,6 @@ private var backgroudColor: some View {
                            blue: 229/255,
                            alpha: 1))
         .ignoresSafeArea()
-}
-
-private func randomSliderValue() -> Double {
-    Double.random(in: 0...255)
 }
 
 struct ContentView_Previews: PreviewProvider {
--- RGBSligerSUI/CustomSliderView.swift
+++ RGBSligerSUI/CustomSliderView.swift
@@ -6,47 +6,37 @@
 //
 
 import SwiftUI
-
+/*
 struct CustomSliderView: View {
-    @Binding var value: Double
-    @Binding var valueText: String
+//    @Binding var value: Double
+//    @Binding var valueText: String
+    let value: Double
     let color: Color
     
     var body: some View {
         HStack(spacing: 8) {
-            TextLabel(textValue: "R: \(lround(value))", colorText: .black)
-            TextLabel(textValue: "0", colorText: color)
-            Slider(value: $value, in: 0...255, step: 1)
+            Text("111")
+                .frame(width: 35, height: 30, alignment: .leading)
+            Slider(value: value, in: 0...255, step: 1)
                 .tint(color)
-            TextLabel(textValue: "255", colorText: color)
-            TextField(valueTextField: $valueText)
+            CustomTextField(valueTextField: $valueText)
         }.padding(.horizontal, 8)
     }
 }
 
-//struct CustomSliderView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CustomSliderView()
-//    }
-//}
-
-struct TextLabel: View {
-    let textValue: String
-    let colorText: Color
-    
-    var body: some View {
-        Text(textValue)
-            .frame(width: 55, height: 30, alignment: .leading)
-            .foregroundColor(colorText)
+struct CustomSliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSliderView(value: <#Double#>, color: .black)
     }
 }
 
-struct TextField: View {
+struct CustomTextField: View {
     @Binding var valueTextField: String
     
     var body: some View {
-        TextField(valueTextField: $valueTextField)
+        TextField("", text: $valueTextField)
             .frame(width: 35, height: 30, alignment: .center)
             .border(.black, width: 1)
     }
 }
+*/
